afcbd247eda5a3437081a72c36e934a4dd5c3b85
dom_Ryan <ryanrodriguues06@gmail.com>
2025-12-14 21:28:42 -0300

    Add: Adicionando Ãlógica na Activity

---
+++ README.md
@@ -0,0 +1,4 @@
+# Aplicativo Android 
+---
+Um simples aplicativo mobile de autenticação. Contém apenas uma página de login, e um pequeno código java para autenticar o usuário.
+S
--- app/src/main/java/com/exemplo/loginpage/MainActivity.java
+++ app/src/main/java/com/exemplo/loginpage/MainActivity.java
@@ -1,6 +1,9 @@
 package com.exemplo.loginpage;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private final String EMAIL = "ryan@gmail.com";
+    private final String SENHA = "rf123456";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+
+    public void autenticar(View view)
+    {
+    EditText email = findViewById(R.id.emailId);
+    String emailFormatado = email.getText().toString();
+
+    EditText senha = findViewById(R.id.passwordId);
+    String senhaFormatado = senha.getText().toString();
+
+    TextView notificacao = findViewById(R.id.notificaçãoId);
+
+    if (emailFormatado.equals(EMAIL) && senhaFormatado.equals(SENHA))
+    {
+        notificacao.setText("Autenticação realizado com sucesso!");
+    }
+    else {
+        notificacao.setText("Autenticação falhou");
+    }
+
+    }
+
 }
